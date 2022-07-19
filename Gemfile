73c7b45ac319afd68feb6444e7ae52069a08454e
Louis <huynguyennbk@gmail.com>
2022-07-19 23:28:11 +0700

    feat: grpc get category list

--- Gemfile
+++ Gemfile
@@ -52,10 +52,21 @@ gem 'bootsnap', require: false
 
 gem 'figaro'
 
+gem 'gruf'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
   gem 'rubocop-rails', require: false
+
+  gem "rspec-rails"
+  gem 'factory_bot_rails'
+  gem 'ffaker', '~> 2.12'
+  gem 'rspec-core'
+  gem 'rspec-expectations'
+  gem 'rspec-mocks'
+  gem 'rspec-support'
+  gem 'shoulda-matchers'
 end
 
 group :development do
@@ -74,15 +85,4 @@ group :test do
   gem 'capybara'
   gem 'selenium-webdriver'
   gem 'webdrivers'
-end
-
-group :development, :test do
-  gem "rspec-rails"
-  gem 'factory_bot_rails'
-  gem 'ffaker', '~> 2.12'
-  gem 'rspec-core'
-  gem 'rspec-expectations'
-  gem 'rspec-mocks'
-  gem 'rspec-support'
-  gem 'shoulda-matchers'
 end
---
+++ app/rpc/categories_controller.rb
@@ -0,0 +1,37 @@
+class CategoriesController < Gruf::Controllers::Base
+  bind Plan::Category::Service
+
+  def get_categories
+    parent_categories = Category
+      .includes(:subcategories)
+      .where(parent_category_id: nil)
+
+    Plan::GetCategoriesResp.new(
+      data: parent_categories.map do |parent|
+        generate_parent_category(parent)
+      end
+    )
+  rescue StandardError => e
+    set_debug_info(e.message, e.backtrace[0..4])
+    fail!(:internal, :internal, "ERROR: #{e.message}")
+  end
+
+  private
+
+  def generate_parent_category(category)
+    Plan::ParentCategory.new(
+      id: category.id,
+      name: category.name,
+      subcategories: category.subcategories.map do |sub|
+        generate_sub_category(sub)
+      end
+    )
+  end
+
+  def generate_sub_category(category)
+    Plan::SubCategory.new(
+      id: category.id,
+      name: category.name,
+    )
+  end
+end
